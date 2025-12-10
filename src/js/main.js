70b98146fa0b9ac8e1df73050d4612ba66976796
ninobirbaum <nino.birbaum@studentfr.ch>
2025-12-10 11:30:49 +0100

    shuffle-letters

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
+import shuffleLetters from "shuffle-letters";
 
+const links = document.querySelectorAll(".suffleletters");
+
+links.forEach((link) => {
+  let isAnimating = false;
+  let canAnimate = true;
+
+  link.addEventListener("mouseenter", () => {
+    if (!isAnimating && canAnimate) {
+      isAnimating = true;
+      canAnimate = false;
+
+      shuffleLetters(link, {
+        iterations: 4,
+        fps: 40,
+      });
+
+      setTimeout(() => {
+        isAnimating = false;
+      }, 800);
+    }
+  });
+
+  link.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+      canAnimate = true;
+    }, 100);
+  });
+});
