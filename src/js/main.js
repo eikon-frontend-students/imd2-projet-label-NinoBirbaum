11bd11747b56213b2be87c832a5eb4d62113bcd2
ninobirbaum <nino.birbaum@studentfr.ch>
2025-12-10 12:18:43 +0100

    cursor "fini"

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,26 @@ links.forEach((link) => {
     }, 100);
   });
 });
+// CUSTOM CURSOR
+
+document.addEventListener("DOMContentLoaded", function () {
+  var cursor = document.createElement("div");
+  cursor.classList.add("custom-cursor");
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", function (e) {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
+  ///.cardscta chamger sur élément qui grandissent
+  var linkElements = document.querySelectorAll(".link, .cards-cta");
+  linkElements.forEach(function (element) {
+    element.addEventListener("mouseenter", function () {
+      cursor.classList.add("scale-up");
+    });
+
+    element.addEventListener("mouseleave", function () {
+      cursor.classList.remove("scale-up");
+    });
+  });
+});
